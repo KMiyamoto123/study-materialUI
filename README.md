ded0c0b20de1aa54a2848da42169f0ff273e557c
kMiyamoto <iarpbs5865@yahoo.co.jp>
2021-03-07 22:48:58 +0900

    #2 study

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 node @15.9.0<br>
 npm @7.5.3<br>
 typescript @4.2.3<br>
+@material-ui/core @4.11.3<br>
 ## プロジェクト作成(初回のみ)
 ```
 npx create-react-app {プロジェクト名} --template typescript --use-npm
+npm install --save @material-ui/core
 ```
--- study_src/src/App.tsx
+++ study_src/src/App.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import MaterialUIStyled from './CustomButton';
+
+// Hooks形式
+const useStyles = makeStyles({
+  button: {
+    backgroundColor: 'green'
+  }
+});
+
+const a = MaterialUIStyled();
+console.log(a)
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <Button variant="contained" className={classes.button}>
+          Hooks式
+        </Button>
+      </div>
+      <div>
+        <MaterialUIStyled></MaterialUIStyled>
+      </div>
+    </>
   );
 }
 
